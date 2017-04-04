9b14f6bb97db0cb833f101f495e6abd08abc613a
rafaelabl <rafaelabl@ufcspa.edu.br>
2017-04-04 17:01:16 -0300

    Added try catch clause
    When the writer inputs a string or invalid character the catch clause will prevent errors

--- src/HellWorld.java
+++ src/HellWorld.java
@@ -3,7 +3,12 @@ import javax.swing.JOptionPane;
 public class HellWorld {
 public static void main(String[] args) {
 	String number = JOptionPane.showInputDialog(null, "Quantas vezes você quer a frase \"Hello World\"?");
-	int num = Integer.parseInt(number);
+	int num = 0;
+	try {
+		num = Integer.parseInt(number);
+	} catch (NumberFormatException e) {
+		e.printStackTrace();
+	}
 	for (int i = 0; i < num; i++) {
 		printHello();
 	}
